e34f112fc7e44dff299fe12f61ebfb31dec93f67
Joel Hobgood <jhobgoo@gmail.com>
2020-03-15 03:38:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,15 +2,14 @@ class Backer
   
   attr_accessor :name, :backed_projects
   
-  @@all = []
   
   def initialize(name)
     @name = name
     @backed_projects = []
-    @@all << self
   end
   
   def back_project(project)
-    project.
+    @backed_project << self
+  end
       
 end
